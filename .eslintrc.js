52c0f34b3581d57806b1f7dab724c24bf3eb696b
paulopkl <palmeida.ipms@gmail.com>
2020-10-11 00:21:31 -0300

    Finished App!

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  root: true,
+  extends: '@react-native-community',
+  'no-trailing-spaces': ["error", { ignoreComments: true }],
+  rules: {
+    'prettier/prettier': 0,
+  }
+};
---
+++ src/components/Display.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+
+const Display = props => {
+    return (
+        <View style={styles.display}>
+            <Text style={styles.displayValue} numberOfLines={1}>{props.value}</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    display: {
+        flex: 1,
+        padding: 20,
+        justifyContent: 'center',
+        backgroundColor: 'rgba(1, 1, 1, 0.6)',
+        alignItems: 'flex-end',
+        width: '100%',
+    },
+    displayValue: {
+        fontSize: 60,
+        color: '#fff'
+    }
+});
+
+export default Display;
